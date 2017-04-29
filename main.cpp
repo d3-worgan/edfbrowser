d6603ff805b62adb14af198997f820e33f4d0e21
Teuniz <teuniz@gmail.com>
2017-04-29 18:01:32 +0200

    Allow to use native menu bar.

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
   QApplication app(argc, argv);
 
-  app.setAttribute(Qt::AA_DontUseNativeMenuBar);
+//   app.setAttribute(Qt::AA_DontUseNativeMenuBar);
 
   QPixmap pixmap(":/images/splash.png");
   QSplashScreen splash(pixmap, Qt::WindowStaysOnTopHint);
