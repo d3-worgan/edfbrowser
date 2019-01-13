098e6a2baf1d23dd4cfeaf683a42e8c802efcc8c
Teuniz <teuniz@gmail.com>
2019-01-13 17:51:23 +0100

    Make sure no segfault can happen when loading two or more files with extreme differences in samplerates  e.g. 200Hz and 250MHz and the pagetime is set too long.

--- viewbuf.cpp
+++ viewbuf.cpp
@@ -224,6 +224,21 @@ void UI_Mainwindow::setup_viewbuf()
       viewbuf = NULL;
     }
 
+    for(i=0, l_temp=0; i<signalcomps; i++)
+    {
+      l_temp += (long long)signalcomp[i]->records_in_viewbuf * (long long)signalcomp[i]->edfhdr->recordsize;
+    }
+
+    if(l_temp >= 0x80000000)
+    {
+      live_stream_active = 0;
+      QMessageBox messagewindow(QMessageBox::Critical, "Error", "Internal error: Memory limit protection:\n\"prefilterbuf\"");
+      messagewindow.exec();
+
+      remove_all_signals();
+      return;
+    }
+
     viewbuf = (char *)malloc(totalsize);
     if(viewbuf==NULL)
     {
@@ -504,6 +519,9 @@ void UI_Mainwindow::setup_viewbuf()
 
     signalcomp[i]->viewbufsize = signalcomp[i]->records_in_viewbuf * signalcomp[i]->edfhdr->recordsize;
 
+//     printf("viewbuf test: signalcomp: %i  records_in_viewbuf: %lli  recordsize: %i  viewbufsize: %i\n",
+//            i, signalcomp[i]->records_in_viewbuf, signalcomp[i]->edfhdr->recordsize, signalcomp[i]->viewbufsize);
+
     signalcomp[i]->samples_on_screen = (int)(((double)pagetime / (double)signalcomp[i]->edfhdr->long_data_record_duration) * (double)signalcomp[i]->edfhdr->edfparam[signalcomp[i]->edfsignal[0]].smp_per_record);
 
     if(signalcomp[i]->edfhdr->viewtime<0)
@@ -572,6 +590,22 @@ void UI_Mainwindow::setup_viewbuf()
   {
     free(viewbuf);
     viewbuf = NULL;
+  }
+
+  for(i=0, l_temp=0; i<signalcomps; i++)
+  {
+    l_temp += (long long)signalcomp[i]->records_in_viewbuf * (long long)signalcomp[i]->edfhdr->recordsize;
+  }
+
+  if(l_temp >= 0x80000000)
+  {
+    live_stream_active = 0;
+    QMessageBox messagewindow(QMessageBox::Critical, "Error", "The system was not able to provide enough resources (memory) to perform the requested action.\n"
+                                                              "Decrease the timescale and try again.");
+    messagewindow.exec();
+
+    remove_all_signals();
+    return;
   }
 
   if(totalsize)
@@ -682,6 +716,13 @@ void UI_Mainwindow::setup_viewbuf()
           if(!skip)
           {
             readsize = dif * signalcomp[i]->edfhdr->recordsize;
+
+//             printf("viewbuf test: signalcomp: %i  viewbufoffset: %i  readsize: %i  records_in_viewbuf: %lli  recordsize: %i\n"
+//                    "viewtime: %lli  datarecords: %lli  dif: %i  readsize: %i  (records_in_viewbuf * recordsize): %lli\n"
+//                    "viewbufsize: %i\n",
+//                    i, signalcomp[i]->viewbufoffset, readsize, signalcomp[i]->records_in_viewbuf, signalcomp[i]->edfhdr->recordsize,
+//                    signalcomp[i]->edfhdr->viewtime, signalcomp[i]->edfhdr->datarecords, dif, readsize,
+//                    signalcomp[i]->records_in_viewbuf * signalcomp[i]->edfhdr->recordsize, signalcomp[i]->viewbufsize);
 
             memset(viewbuf + signalcomp[i]->viewbufoffset + readsize, 0, (signalcomp[i]->records_in_viewbuf * signalcomp[i]->edfhdr->recordsize) - readsize);
           }
